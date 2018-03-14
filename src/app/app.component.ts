752a9fe1e566c9613469592dd14a7b948c1c4be0
RAJSARAOGI\Raj Saraogi <saraogi.raj40@gmail.com>
2018-03-14 19:26:19 +0530

    added basic bindings in the form

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  defaultQuestion="pet";
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
