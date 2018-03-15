4cc5c39d459b30850eac2e9e9eb4f33049f210cd
RAJSARAOGI\Raj Saraogi <saraogi.raj40@gmail.com>
2018-03-15 21:42:40 +0530

    added validators in rc

--- src/app/rc/rc.component.ts
+++ src/app/rc/rc.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rc',
@@ -13,8 +13,8 @@ export class RcComponent implements OnInit {
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl(null),
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null,[Validators.required, Validators.email]),
       'gender': new FormControl('Male'),
     });
   }
