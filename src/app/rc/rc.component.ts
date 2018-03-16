c93482fcd2f7425bd8856a3b06e721bc4f42d8b2
RAJSARAOGI\Raj Saraogi <saraogi.raj40@gmail.com>
2018-03-16 13:07:56 +0530

    added async custom validator to rc

--- src/app/rc/rc.component.ts
+++ src/app/rc/rc.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,  Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-rc',
@@ -14,12 +15,27 @@ export class RcComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null,[Validators.required, Validators.email]),
+      'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
       'gender': new FormControl('Male'),
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resovle, reject) => {
+      setTimeout(() => {
+        if (Control.value === 'test@test.com') {
+          resovle({
+            'emailIsForbidden': true
+          });
+        } else {
+          resovle(null);
+        }
+      }, 2000);
+    });
+    return promise;
   }
 }
